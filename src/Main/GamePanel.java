0fd66b950387fa8df365d08c2621a3d862d7b7ea
tooshfty <fergusonryan1998@gmail.com>
2025-09-11 15:44:18 -0700

    first

---
+++ src/Main/GamePanel.java
@@ -0,0 +1,98 @@
+package Main;
+
+import entity.Player;
+import tile.TileManager;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable{
+
+    final int ogTileSize = 16; // 16x16 tile
+    final int scale = 3;
+
+    public final int tileSize = ogTileSize * scale; //48x48 tile
+
+    //maintain 4:3 ratio
+    public final int maxScreenCol = 16; // 16 horizontal tiles
+    public final int maxScreenRow = 12; // 12 vertical tiles
+    public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    //FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
+
+    TileManager tileM = new TileManager(this);
+    Thread gameThread;
+    Player player = new Player(this,keyH);
+
+
+    //GamePanel constructor
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread(){
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    @Override
+    public void run() {
+        double drawInterval = 1000000000 / FPS; //1 billion nanoseconds over 60fps is updating every .0166 seconds
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null) {
+
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if (timer > 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    public void update(){
+
+        player.update();
+    }
+    public void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+        tileM.draw(g2);
+        player.draw(g2);
+
+        g2.dispose();
+        
+    }
+
+
+
+
+
+
+}
---
+++ src/tile/Tile.java
@@ -0,0 +1,18 @@
+package tile;
+
+import java.awt.image.BufferedImage;
+
+public class Tile {
+
+
+
+    public BufferedImage image;
+    public boolean collision = false;
+
+
+
+
+
+
+
+}
